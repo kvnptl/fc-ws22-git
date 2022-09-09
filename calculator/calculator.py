69500b9f92d2bb42c17f3fcf5eae6eec3ad65dc0
AashishKumbashi <aashish.kumbashi@gmail.com>
2022-09-09 16:22:26 +0200

    Add exponential and nth root

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -7,7 +7,15 @@ Email: ravisankar@dummygmail.com
 def initilization():
 	print("Initiating Calculator ...")
 
-	
+def exponential(a,b):
+	"""Calculates the nth power and nth root"""
+	try:
+		power = a**b
+		root = a*(1/b)
+		return(power,root)
+	except Exception as e:
+		print("Something went wrong. Reason: "+e)
+
 
 
 def main():
