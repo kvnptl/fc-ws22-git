30638ced39b034d6cb7acbd90025581917bfc957
Omar Aboelmagd <omar.aboelmagd@smail.inf.h-brs.de>
2022-09-09 16:09:32 +0200

    Added the rest of arithmetics

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -4,16 +4,26 @@ Date: 09.09.2022
 Email: ravisankar@dummygmail.com
 """
 
-def add(a, b):
-	return a + b
+add = lambda a, b: a + b
+substract = lambda a, b: a - b
+multiply = lambda a, b: a * b
+def division(a, b):
+	if b == 0: raise Exception("Can't divide by 0!")
+	return a / b
 
 def initilization():
 	print("Initiating Calculator ...")
-	operation = input("> Choose an operation (+): ")
+	operation = input("> Choose an operation (+, -, *, /): ")
 	number_1 = int(input("> Enter Num 1: "))
 	number_2 = int(input("> Enter Num 2: "))
 	if operation == "+":
 		print(f"* Result is {add(number_1, number_2)}")
+	elif operation == "-":
+		print(f"* Result is {substract(number_1, number_2)}")
+	elif operation == "*":
+		print(f"* Result is {multiply(number_1, number_2)}")
+	elif operation == "/":
+		print(f"* Result is {division(number_1, number_2)}")
 	
 
 
