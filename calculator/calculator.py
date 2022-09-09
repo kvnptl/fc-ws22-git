7857b1e1499d49eed329c0f30b65c6df940e2644
khatina11 <khatinasari@gmail.com>
2022-09-09 16:24:12 +0200

    Number one

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -9,6 +9,15 @@ def initilization():
 
 	
 
+def one():
+	print("Enter Number: ")
+	x= input()
+	ex = math.exp(x)
+	log2 = math.log2(x)
+	log10 = math.log10(x)
+	print("The exponential value is " + ex)
+	print("The Log2 value is " + log2)
+	print("The log10 value is " +log10)
 
 def main():
 	initilization()
