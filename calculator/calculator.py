7727106473ce8caf18b9f239b66efa2714884a20
Omar Aboelmagd <omar.aboelmagd@smail.inf.h-brs.de>
2022-09-09 15:55:01 +0200

    Added a sum operation

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -4,12 +4,16 @@ Date: 09.09.2022
 Email: ravisankar@dummygmail.com
 """
 
+def add(a, b):
+	return a + b
+
 def initilization():
 	print("Initiating Calculator ...")
-	operation = input("Choose an operation: ")
-	number_1 = int(input("Enter Num 1: "))
-	number_2 = int(input("Enter Num 2: "))
-
+	operation = input("> Choose an operation (+): ")
+	number_1 = int(input("> Enter Num 1: "))
+	number_2 = int(input("> Enter Num 2: "))
+	if operation == "+":
+		print(f"* Result is {add(number_1, number_2)}")
 	
 
 
