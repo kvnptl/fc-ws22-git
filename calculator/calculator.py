e2cd0a6996c7c6562ed75e0557d599147193ae90
Omar Aboelmagd <omar.aboelmagd@smail.inf.h-brs.de>
2022-09-09 15:53:37 +0200

    Added input parameters

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -6,6 +6,9 @@ Email: ravisankar@dummygmail.com
 
 def initilization():
 	print("Initiating Calculator ...")
+	operation = input("Choose an operation: ")
+	number_1 = int(input("Enter Num 1: "))
+	number_2 = int(input("Enter Num 2: "))
 
 	
 
