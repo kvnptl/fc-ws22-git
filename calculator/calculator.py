3fec5a0b8b23ae7a2fed16b78269f64d16ad1362
Sebi13412 <sebastianmarian.iliescu@gmail.com>
2022-09-09 16:56:39 +0200

    Applied the feature

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -12,7 +12,20 @@ def initilization():
 
 def main():
 	initilization()
-
+	
+number 1=float(input"Enter first number:"))
+number 2=float(input("Enter second number:"))
+def modulo(a,b)
+	modulo= a%b
+	return modulo 
+	print("result is:",modulo(number 1, number2))
+	
+number 1=float(input"Enter first number:"))
+number 2=float(input("Enter second number:"))
+def division(c,d)
+ 	div= c/d
+ 	return div
+ print ("result is:",division(number 1, number2))
 
 
 main()
