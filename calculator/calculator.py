c87cb8491d7788fae5e7c593ac160124769f9892
Mohammed Waseem <mohammedwaseem019@gmail.com>
2022-09-09 16:48:00 +0200

    updated

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -7,10 +7,12 @@ Email: ravisankar@dummygmail.com
 def initilization():
 	print("Initiating Calculator ...")
 
+# function to calculate area of circle
 def area_of_circle(radius):
 	area = 3.14 * radius * radius
 	print("area of circle = %.2f" %area)
 
+# function to calculate circumference of circle
 def circumference_of_circle(radius):
 	circumference = 2 *3.14 * radius	
 	print("circumference of circle = %.2f" %circumference)
