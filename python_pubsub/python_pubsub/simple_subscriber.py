3530fc9cec5d0e8b2f751417d5c886904be0e02b
Mohammed Waseem <mohammedwaseem019@gmail.com>
2022-09-13 16:31:47 +0200

    addind ROS2 pubsub

---
+++ python_pubsub/python_pubsub/simple_subscriber.py
@@ -0,0 +1,26 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class SimpleSubscriber(Node):
+    def __init__(self):
+        super(). __init__("simple_subscriber")
+        self.subscription = self.create_subscription(
+            String,
+            '/physics',
+            self.sub_callback,
+            10
+        )
+
+    def sub_callback(self, msg):
+        self.get_logger().info(f'Multiverse heard {msg.data}')
+
+def main(args=None):
+    rclpy.init(args=args)
+    simple_subscriber = SimpleSubscriber()
+    rclpy.spin(simple_subscriber)
+    simple_subscriber.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
