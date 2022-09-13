d8bfefd64faaa0d7da27f3dd05eb35199ea2c643
Sebi13412 <sebastianmarian.iliescu@gmail.com>
2022-09-13 11:56:40 +0200

    created animal class with monkey

---
+++ monkey.py
@@ -0,0 +1,29 @@
+from animal import Animal
+#animal the name of the file where the class is in
+
+class Monkey (Animal):
+
+    '''
+    Params: name, height , is_predator , monkey_type , color m country
+    '''
+    
+    def __init(self, name, height, is_predator, monkey_type, color, country):
+        super().__init__(name,height,is_predator)
+        self.moneky_type = monkey_type
+        self.color = color
+        self.country = country
+
+    #polymorphism - overriding parent class method
+    def sound(self, scream_duration):
+        self.sound_type = 'chatter'
+        self.Animal__name= 'new monkey'
+        print(f'I can scream for {scream_duration} seconds')
+
+    def jump(self, jump_height):
+        print(f'I can jump{jump_height} feet')
+
+#instaciate - enter an object
+
+chimp = Monkey('monkey',4 , False,'chimp', 'brown','india')
+chimp.sound(2)
+print(chimp)
