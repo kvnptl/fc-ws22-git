82d07cb42f6f153bf732e818fc47672fdac23a87
AashishKumbashi <aashish.kumbashi@gmail.com>
2022-09-15 16:11:47 +0200

    refactor with docstring and function seperation

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -7,15 +7,29 @@ Email: ravisankar@dummygmail.com
 def initilization():
 	print("Initiating Calculator ...")
 
-def exponential(a,b):
-	"""Calculates the nth power and nth root"""
+def nth_power(a,b):
+	"""Calculates the a to the power of b
+	Args:
+		a - int
+		b - int
+	"""
 	try:
 		power = a**b
-		root = a*(1/b)
-		return(power,root)
+		return(power)
 	except Exception as e:
 		print("Something went wrong. Reason: "+e)
 
+def nth_root(a,b):
+	"""Calculates the bth root of a
+	Args:
+		a - int
+		b - int
+	"""	
+	try:
+		root = a*(1/b)
+		return(root)
+	except Exception as e:
+		print("Something went wrong. Reason: "+e)
 
 
 def main():
