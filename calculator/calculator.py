4712ac00d2b959201ad9176857f38187f635fbab
khatina11 <khatinasari@gmail.com>
2022-09-16 09:49:18 +0200

    revising my calculator assignment

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,27 +1,58 @@
+import math
 """
 Author: Ravisankar
 Date: 09.09.2022
 Email: ravisankar@dummygmail.com
 """
 
+#program to calculate exponential, log base 2, and log base 10
 def initilization():
-	print("Initiating Calculator ...")
+    print("Initiating Calculator ...")
 
-	
 
-def one():
-	print("Enter Number: ")
-	x= input()
-	ex = math.exp(x)
-	log2 = math.log2(x)
-	log10 = math.log10(x)
-	print("The exponential value is " + ex)
-	print("The Log2 value is " + log2)
-	print("The log10 value is " +log10)
+#this function count the exponential of a number
+def exponential(x, n):
+    return pow(x, n)
+
+#function for log base 2
+def log_2(x):
+    return math.log2(x)
+
+#function for log base 10
+def log_10(x):
+    return math.log10(x)
+
 
 def main():
-	initilization()
-
+    initilization()
 
 
 main()
+
+#mesagges to show options and take input
+print("Calculating options:")
+print("1.Exponential")
+print("2.Log base 2")
+print("3.Log base 10")
+
+option = input("Option number: ")
+
+if option in ('1', '2', '3'):
+    x = int(input("Enter any number: "))
+
+    if option == '1':
+        n = int(input("To the power of: "))
+        print(x, "^", n, "=", exponential(x, n))
+    
+    elif option == '2':
+        print(x, "Log 2 =", log_2(x))
+        
+    elif option == '3':
+        print(x, "Log 10 =", log_10(x))
+   
+            
+else:
+    print("Try again!")
+        
+
+
